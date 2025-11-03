0da498f0b10adc5403c47ddf237410fe17bf31b9
unknown <simarti.rani@cloudprimero.com>
2025-11-04 00:38:05 +0500

    Initial Commit

---
+++ react-app/src/App.jsx
@@ -0,0 +1,32 @@
+import {react} from 'react'
+import {createRoot} from 'react-dom/client'
+function App() {
+  return (
+    /*<div className="headd">
+      <h1>Hello World</h1>
+    </div>
+
+  );*/
+hello = () => "Hello World";
+}
+  
+/*const myElement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>Simrti</td>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+  </table>
+);
+
+
+createRoot(document.getElementById('root')).render(
+  myElement
+
+) */
+export default App;
